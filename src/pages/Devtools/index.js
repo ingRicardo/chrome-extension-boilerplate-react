04c92238f1e5ea13c0239b5a444a44bee16f9675
Riky <ra_mo116@hotmail.com>
2025-04-15 08:14:08 -0700

    mods to dev tools panel

--- src/pages/Devtools/index.js
+++ src/pages/Devtools/index.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create(
-  'Dev Tools from chrome-extension-boilerplate-react',
+  'Dev Tools from chrome-extension-boilerplate-react V1.0',
   'icon-34.png',
   'panel.html'
 );
--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Panel.css';
 
+var links = [
+  { endpoint: '/america' },
+  { endpoint: '/canada' },
+  { endpoint: '/norway' },
+  { endpoint: '/bahamas' }
+]
+
 const Panel: React.FC = () => {
+
+
+  const listItems = links.map((link) =>
+    <li key={link.endpoint}>{link.endpoint}</li>
+  );
+
   return (
     <div className="container">
-      <h1>Dev Tools Panel</h1>
+      <h1>Dev Tools Panel v1</h1>
+      <p>Riky</p>
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 };
