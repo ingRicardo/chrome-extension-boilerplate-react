9e0921ffcdf9aaf95c5c694930bb8f7c91733540
Riky <ra_mo116@hotmail.com>
2025-04-18 08:43:09 -0700

    func to display orinal json, make variable modified and async call var added

--- src/background.js
+++ src/background.js
@@ -1,11 +1,11 @@
 
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
+  function (details) {
     // Optional: Filter requests based on URL or other criteria
-  //   if (details.url.includes("https://api.github.com/users/hadley/orgs")) {
- //     console.log("Intercepting request:", details);
- //    }
+    //   if (details.url.includes("https://api.github.com/users/hadley/orgs")) {
+    //  console.log("Intercepting request:", details.url);
+    //    }
     return {}; // Continue the request
   },
   { urls: ["<all_urls>"] },
@@ -13,7 +13,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
+  function (details) {
     // Check if the Content-Type indicates a JSON response
     const contentTypeHeader = details.responseHeaders.find(header => header.name.toLowerCase() === 'content-type');
     if (contentTypeHeader && contentTypeHeader.value.toLowerCase().includes('application/json')) {
@@ -26,39 +26,47 @@ chrome.webRequest.onHeadersReceived.addListener(
   ["responseHeaders"]
 );
 
-
-
+var count = 0;
 chrome.webRequest.onCompleted.addListener(
   async function myListener(details) {
     const contentTypeHeader = details.responseHeaders?.find(header => header.name.toLowerCase() === 'content-type');
     if (contentTypeHeader && contentTypeHeader.value.toLowerCase().includes('application/json')) {
       try {
-        
+        console.log("Intercepting request:", details.url);
+
+        fetch(details.url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Original JSON:", data);
+          })
+          .catch(console.error);
+
         const response = await fetch(details.url);
         let responseBody = await response.json();
-  
+
         // --- Your JSON modification logic here ---
-        console.log("Original JSON:", responseBody);
-  
         // Example: Add a new field
         responseBody.modifiedByExtension = true;
-  
+
         // Example: Modify an existing field
         if (responseBody[0].model) {
           responseBody[0].model = "Modified Value by Extension";
+          responseBody[0].make = "MERCEDES BENZ";
         }
-  
+
         console.log("Modified JSON:", responseBody);
-        const modifiedBody = JSON.stringify(responseBody);
-  
+        
+
       } catch (error) {
         console.error("Error processing JSON:", error);
       }
+      count++;
+   
+      console.log("async call :" + count);
     }
+
     // IMPORTANT: Remove the listener when done
-
-      chrome.webRequest.onCompleted.removeListener(myListener);
-
+    chrome.webRequest.onCompleted.removeListener(myListener);
   },
   { urls: ["<all_urls>"] },
   ["responseHeaders"]
