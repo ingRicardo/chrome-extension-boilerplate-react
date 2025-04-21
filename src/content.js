f36f9f9e860533f554ce18eb9127ca49ef3d06a3
Riky <ra_mo116@hotmail.com>
2025-04-21 15:40:21 -0700

    working with xmlhhtprequests

---
+++ src/content.js
@@ -0,0 +1,56 @@
+(function() {
+  console.log("Content script loaded"+ window.location.href);
+
+  const originalXHR = window.XMLHttpRequest;
+  window.XMLHttpRequest = function() {
+    const xhr = new originalXHR();
+    const originalSend = xhr.send;
+    xhr.send = function(...args) {
+      console.log("XHR send called with arguments:", args); // Log the arguments for debugging
+      const onload = this.onload;
+
+      this.onload = function() {
+        if (this.responseURL.includes('https://conisoft.org/cakes/loadvehicles.php') && this.getResponseHeader('Content-Type')?.includes('application/json')) {
+          try {
+            const originalResponse = JSON.parse(this.responseText);
+            const modifiedResponse = modifyMyJson(originalResponse);
+            Object.defineProperty(this, 'response', { writable: true, value: JSON.stringify(modifiedResponse) });
+            Object.defineProperty(this, 'responseText', { writable: true, value: JSON.stringify(modifiedResponse) });
+          } catch (error) {
+            console.error("Error modifying JSON in XHR:", error);
+          }
+        }
+        if (onload) {
+          onload.apply(this, arguments);
+        }
+      };
+      originalSend.apply(this, args);
+    };
+    console.log("XHR created:", xhr); // Log the XHR object for debugging
+    //xhr.overrideMimeType("text/plain; charset=x-user-defined");
+
+    return xhr;
+  };
+
+
+  function modifyMyJson(json) {
+    // Example modification: Adding a new property
+    json.modifiedByExtension = true;
+    json[0].make = "Modified by declarativeNetRequest!";
+    if (json.items && Array.isArray(json.items)) {
+      json.items.forEach(item => {
+        item.make = "Processed by declarativeNetRequest!";
+      });
+    }
+    console.log("Modifying JSON:", json);
+
+    return json;
+  }
+
+const xhr = new XMLHttpRequest();
+xhr.overrideMimeType("text/plain");
+xhr.open('GET', window.location.href);
+xhr.send();
+
+
+})();
