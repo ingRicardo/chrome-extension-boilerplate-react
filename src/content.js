725888edbd8d994a41c7fefd03cd332534c14e4a
Riky <ra_mo116@hotmail.com>
2025-04-22 09:59:34 -0700

    changed logic to decode and encode data

--- src/content.js
+++ src/content.js
@@ -1,49 +1,38 @@
 (function() {
-  console.log("Content script loaded"+ window.location.href);
 
-  const originalXHR = window.XMLHttpRequest;
-  window.XMLHttpRequest = function() {
-    const xhr = new originalXHR();
-    const originalSend = xhr.send;
-    xhr.send = function(...args) {
-      console.log("XHR send called with arguments:", args); // Log the arguments for debugging
-      const onload = this.onload;
+  window.onload = function (event) {
+ 
+      fetch(window.location.href).then(response => {
+        console.log(window.location.href);
+          const reader = response.body.getReader();
+          const decoder = new TextDecoder();
+          const encoder = new TextEncoder();
+          const stream = new ReadableStream({
+              start(controller) {
+                  function read() {
+                      reader.read().then(({ done, value }) => {
+                          if (done) {
+                              controller.close();
+                              return;
+                          }
+                          // Modify the response data here
+                          let text = decoder.decode(value, { stream: true });
+                          text = text.replace(/"fgfgfgfg"/i, 'bar');
+                          console.log("text mod -->"+text); // Process chunk
+                          var res= encoder.encode(text);
+               
+                          controller.enqueue(res);
+                          return  read();
+                      });
+                  }
+                 return read();
+              }
+          });
+          return new Response(stream, { headers: response.headers });
+          
+      })
 
-      this.onload = function() {
-        if (this.responseURL.includes('https://conisoft.org/cakes/loadvehicles.php') && this.getResponseHeader('Content-Type')?.includes('application/json')) {
-          try {
-            const originalResponse = JSON.parse(this.responseText);
-            const modifiedResponse = modifyMyJson(originalResponse);
-            Object.defineProperty(this, 'response', { writable: true, value: JSON.stringify(modifiedResponse) });
-            Object.defineProperty(this, 'responseText', { writable: true, value: JSON.stringify(modifiedResponse) });
-          } catch (error) {
-            console.error("Error modifying JSON in XHR:", error);
-          }
-        }
-        if (onload) {
-          onload.apply(this, arguments);
-        }
-      };
-      originalSend.apply(this, args);
-    };
-    console.log("XHR created:", xhr); // Log the XHR object for debugging
-
-    return xhr;
   };
 
-
-  function modifyMyJson(json) {
-    // Example modification: Adding a new property
-    json.modifiedByExtension = true;
-    json[0].make = "Modified by declarativeNetRequest!";
-    console.log("Modifying JSON:", json);
-    return json;
-  }
-
-const xhr = new XMLHttpRequest();
-xhr.overrideMimeType("text/plain");
-xhr.open('GET', window.location.href);
-xhr.send();
-
-
+ 
 })();
