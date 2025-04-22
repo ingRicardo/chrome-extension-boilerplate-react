37e9a49e8974c43f333767ffc99179975c6c0fa2
Riky <ra_mo116@hotmail.com>
2025-04-22 16:57:14 -0700

    worked with onBeforerequest method

--- src/background.js
+++ src/background.js
@@ -1,30 +1,104 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    // Optional: Filter requests based on URL or other criteria
-    //   if (details.url.includes("https://api.github.com/users/hadley/orgs")) {
-    //  console.log("Intercepting request:", details.url);
-    //    }
-    return {}; // Continue the request
-  },
-  { urls: ["<all_urls>"] },
-  ["requestBody"] // If you need to modify request body, include this
-);
+
+let jsonval=null
+
+fetch("https://conisoft.org/cakes/loadvehicles.php")
+  // Retrieve its body as ReadableStream
+  .then((response) => response.body)
+  .then((body) => {
+    const reader = body.getReader();
+    const decoder = new TextDecoder();
+    const encoder = new TextEncoder();
+    return new ReadableStream({
+      start(controller) {
+        return pump();
+
+        function pump() {
+          return reader.read().then(({ done, value }) => {
+            // When no more data needs to be consumed, close the stream
+            if (done) {
+              controller.close();
+              return;
+            }
+            // Modify the response data here
+            let text = decoder.decode(value, { stream: true });
+            text = text.replace(/"fgfgfgfg"/i, '"bar"');
+            console.log("text mod -->"+text); // Process chunk
+          //  jsonval=text;
+            var res= encoder.encode(text);
+            jsonval= res;
+            // Enqueue the next data chunk into our target stream
+            controller.enqueue(res);
+            return pump();
+          });
+        }
+      },
+    });
+  })
+  .then((stream) => new Response(stream))
+  .then((response) => response.blob())
+  .catch((err) => console.error(err));
 
 
- 
+const targets = {urls: ['https://conisoft.org/cakes/loadvehicles.php'], types: ['xmlhttprequest']};
 
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
-    // Check if the Content-Type indicates a JSON response
-    const contentTypeHeader = details.responseHeaders.find(header => header.name.toLowerCase() === 'content-type');
+  // Check if the response content type is JSON
+  const contentTypeHeader = details.responseHeaders.find(header => header.name.toLowerCase() === 'content-type');
     if (contentTypeHeader && contentTypeHeader.value.toLowerCase().includes('application/json')) {
-      // Initiate the process of reading the response body
-      return { responseHeaders: details.responseHeaders };
+    // Store the response headers for later use
+      chrome.storage.local.set({ [`responseHeaders-${details.requestId}`]: details.responseHeaders });
+
+      console.log("Storing headers for requestId:", details.requestId, "Headers:", details.responseHeaders);
+     // console.log(">=== " + chrome.storage.local.get([`responseHeaders-${details.requestId}`]));
+      return { responseHeaders: details.responseHeaders }; // Let the headers pass through
     }
-    return {};
   },
   { urls: ["<all_urls>"] },
   ["responseHeaders"]
+);
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+   function myListener(details) {
+
+        try {
+          const decoder = new TextDecoder();
+          console.log("details ==> ", details);
+ 
+          if(details.response){
+            console.log("response body ==> ", details.response.body);
+          }
+          if(details.requestBody){
+            var postedString = decodeURIComponent(String.fromCharCode.apply(null,
+              new Uint8Array(details.requestBody.raw[0].bytes)));
+            console.log("Request Body:", postedString);
+          }
+          const responseBody = decoder.decode( jsonval);
+          const jsonBody = JSON.parse(responseBody);
+
+          // Modify the JSON body here
+        //  console.log("Original JSON:", jsonBody);
+          jsonBody.modified = true;
+          jsonBody[0].make = "This value has been changed!";
+
+          const encoder = new TextEncoder();
+          const modifiedResponseBody = encoder.encode(JSON.stringify(jsonBody));
+
+
+          console.log("Modified JSON:", modifiedResponseBody);
+      //    return { response: modifiedResponseBody };
+        } catch (error) {
+          console.error("Error parsing or modifying JSON:", error);
+          // Optionally, you might want to return the original response here
+        //  return { response: details.response.body };
+        }
+        
+        return {};
+
+  },
+  targets,
+  ["blocking","requestBody"]
 );
 
 var count = 0;
