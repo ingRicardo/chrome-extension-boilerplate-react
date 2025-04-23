d00a21ea7b845c73d7932c409b4518ec62d2227c
Riky <ra_mo116@hotmail.com>
2025-04-23 16:18:17 -0700

    removed logs

--- src/content.js
+++ src/content.js
@@ -2,28 +2,22 @@
 
     console.log("Content script loaded!",window.location.href);
   
-  
     const originalFetch = window.fetch;
-      console.log("Original fetch function:", originalFetch);
       window.fetch = async (...args) => {
-  
-  
-      console.log("Fetch called with arguments:", args);
-  
       const response = await originalFetch(...args);
-          console.log("originalFetch Original response:", response);
-         // const clonedResponse = response.clone();
           response.text().then(text => {
-              console.log("originalFetch Original Response:", text);
               if (text.includes("model")) { // Modify based on JSON structure
                   const modifiedText = text.replace("fgfgfgfg", "newValue");
-                  console.log("originalFetch Modified Response:", modifiedText);
+                  console.log(" Modified Response:", modifiedText);
+                     
               }
           });
           return response;
       };
       window.fetch(window.location.href)
-  
+
+             
+             
   
   
   
